e977d47d55dd52b3909ff229ad98cbefd29399a8
MariusTBjercke <marius@bjerckemedia.no>
2021-09-10 12:04:41 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+<script>
+// model
+var pistol 
+var kniv 
+var bumbe 
+
+// view
+        updateView();
+        function updateView() {
+        document.getElementById('app').innerHTML = 
+        <button onclick="pistol()"></button>
+        <button onklick="bombe()"></button>
+        <button onklick="kniv()"></button>
+
+        }
+
+// controller
+function OnClick() {
+
+}
+
+
+</script>
