1d6daff8b67209ec7e0cebf2c5a6e0c75c95065c
MariusTBjercke <marius@bjerckemedia.no>
2021-09-13 14:55:14 +0200

    Rewrote functionalities for game

--- js/main.js
+++ js/main.js
@@ -2,79 +2,125 @@
 let botChoice; 
 let playerChoice;
 let kpb = ["kniv", "pistol", "bombe"];
-let result;
-let bombe = "img/bomb.png";
-let pistol = "img/gun-png.png";
-let kniv = "img/kniv.png";
+let round = 1;
+let tieText = "";
+// 0 = player, 1 = bot
+let players = [0, 0];
 //Hjelpevariabel for både view og controller
 let main = document.querySelector("main");
 
-
-
 //view
+show();
+function show() {
 
+    // Reset HTML og utfør alt under på nytt
+    main.innerHTML = '';
 
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "header-wrapper");
 
+    const headerTitle = document.createElement("h1");
+    headerTitle.innerText = "Kniv, pistol, bombe.";
+
+    const subTitle = document.createElement("h2");
+    subTitle.innerText = "Laget av team 1.";
+
+    const roundText = document.createElement("h3");
+    roundText.innerText = "Runde: " + round;
+
+    const playerScore = document.createElement("h3");
+    playerScore.innerText = "Spiller: " + players[0];
+    
+    const botScore = document.createElement("h3");
+    botScore.innerText = "Bot: " + players[1];
+    
+    const weaponsTitle = document.createElement("h4");
+    weaponsTitle.innerText = "Velg et våpen:";
+
+    const weaponsContainer = document.createElement("div");
+    weaponsContainer.setAttribute("id", "weapons-container");
+    weapons = weaponsContainer;
+
+    const kniv = document.createElement("div");
+    kniv.setAttribute("id", "kniv");
+
+    const pistol = document.createElement("div");
+    pistol.setAttribute("id", "pistol");
+
+    const bombe = document.createElement("div");
+    bombe.setAttribute("id", "bombe");
+
+    const tie = document.createElement("h2");
+    tie.innerText = tieText;
+
+    // Setup HTML elements
+    main.appendChild(wrapper);
+    wrapper.appendChild(headerTitle);
+    wrapper.appendChild(subTitle);
+    wrapper.appendChild(roundText);
+    wrapper.appendChild(playerScore);
+    wrapper.appendChild(botScore);
+    wrapper.appendChild(weaponsTitle);
+    wrapper.appendChild(weaponsContainer);
+    weaponsContainer.appendChild(kniv);
+    weaponsContainer.appendChild(pistol);
+    weaponsContainer.appendChild(bombe);
+    wrapper.appendChild(tie);
+
+    // Events
+    weaponsContainer.addEventListener("click", playRound, false);
+}
 
 //controller
 
-const random = Math.floor(Math.random() * kpb.length);
-console.log(random, kpb[random]);
+function playRound(e) {
+
+    const random = Math.floor(Math.random() * kpb.length);
+    playerChoice = e.target.id;
+    botChoice = kpb[random];
+    getResult(playerChoice, botChoice);
+
+    show();
+
+}
 
 
 
 // Kniv banker bombe
 // pistol banker kniv
 // bombe banker pistol
-function getResult() {
+function getResult(player, bot) {
   
     //spiller sine valg
-    if ( playerChoice === "kniv" && botChoice === "bombe") {
-       
-    } else if (playerChoice === "kniv" && botChoice === "pistol") {
-        result = "Du vant"
-    } else if ( playerChoice === "bombe" && botChoice === "pistol") {
-        result = "Du vant"
-    } else if ( playerChoice === "pistol" && botChoice === "kniv") {
-        result = "Du vant"
+    if (player === bot) {
+        tieText = "Det ble uavgjort!";
+    }
+    else if (player == 'kniv') {
+        if (bot == 'pistol') {
+            players[1]++;
 
-
-    //bot sine valg
-    } else if (botChoice === "kniv" && playerChoice === "pistol") {
-        result = "Du tapte, prøv igjen"
-    } else if ( botChoice === "bombe" && playerChoice === "pistol") {
-        result = "Du tapte, prøv igjen"
-    } else if ( botChoice === "pistol" && playerChoice === "kniv") {
-        result = "Du tapte, prøv igjen"
-
-    } else (playerChoice === botChoice) {
-        result = "Uavgjort"
+        } else {
+            players[0]++;
+        }
+        tieText = "";
+    }
+    else if (player == 'bombe') {
+        if (bot == 'kniv') {
+            players[1]++
+        } else {
+            players[0]++
+        }
+        tieText = "";
+    }
+    else if (player == 'pistol') {
+        if (bot == 'bombe') {
+            players[1]++
+        } else {
+            players[0]++
+        }
+        tieText = "";
     }
 
-        
+    round++;
 
-
-
-
-
-
-
-
- 
-// function setBotChoice()      {
-//     let blankIndexes = Array.from(board.keys()).filter(i => board[i] === '');
-//     if (blankIndexes.length === 0) return;
-//     let random = Math.floor(Math.random() * blankIndexes.length);
-//     let blankIndex = blankIndexes[random];
-//     board[blankIndex] = 'o';
-
-
-//     if (you===computer){
-//          console.log ("It's a tie!");
-//     } else if (you==="bombe" && computer === "kniv" || you === "pistol" && computer==="bombe"||you==="kniv"&&computer==="Pistol") {
-//          console.log ("You've won!");
-//     } else {
-//          console.log ("You've lost!");
-    // }
-// }
-    
+}
